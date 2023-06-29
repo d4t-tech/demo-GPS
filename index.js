63ab0cb090fd503e581d0908f4c6937d84c8701e
kevd4t <k.blanco@d4t.tech>
2023-06-29 16:05:12 -0400

    new update

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import express from 'express'
-import { HandleErrors, NotFound, handleGPSTracking } from './gps/controller.js'
 
 const server = express()
 
 server.set('port', process.env.PORT || 3500)
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }))
+
+const handleGPSTracking = (req, res) => {
+  return res.status(200).json({
+    message: 'fino papu'
+  })
+}
+
+const NotFound = (req, res) => {
+  res.status(204).json({
+    message: 'Ruta no encontrada porfavor ingrese a /api'
+  })
+}
+
+const HandleErrors = (err, req, res, next) => {
+  console.error(err)
+  res.status(500).json({
+    message: 'Algo salio mal!'
+  })
+}
 
 server.get('/', handleGPSTracking)
 server.use(NotFound)
