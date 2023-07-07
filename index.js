71376fe23164f7974f1a20ad339c0bdf595ed917
kevd4t <k.blanco@d4t.tech>
2023-07-07 09:33:45 -0400

    adding --watch + allow all methods http

--- index.js
+++ index.js
@@ -7,25 +7,34 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }))
 
 const handleGPSTracking = (req, res) => {
+  console.log({ body: req.body })
+  console.log({ queryParam: req.query })
+
   return res.status(200).json({
-    message: 'fino papu'
+    body: req.body,
+    queryParam: req.query
   })
 }
 
 const NotFound = (req, res) => {
   res.status(204).json({
-    message: 'Ruta no encontrada porfavor ingrese a /api'
+    message: 'Ruta no encontrada porfavor ingrese a /'
   })
 }
 
 const HandleErrors = (err, req, res, next) => {
   console.error(err)
   res.status(500).json({
-    message: 'Algo salio mal!'
+    message: 'Algo salio mal!',
+    error: err
   })
 }
 
 server.get('/', handleGPSTracking)
+server.post('/', handleGPSTracking)
+server.put('/', handleGPSTracking)
+server.delete('/', handleGPSTracking)
+server.patch('/', handleGPSTracking)
 server.use(NotFound)
 server.use(HandleErrors)
 
